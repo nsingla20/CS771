096abd9cf075ec504d34ad2c4c8706219d605eda
nsingla20 <nsingla20@iitk.ac.in>
2023-02-17 16:53:43 +0530

    ass1 update

--- assn1/submit copy.py
+++ assn1/submit copy.py
@@ -17,7 +17,8 @@ from sklearn.linear_model import LogisticRegression
 # For example, functions to calculate next coordinate or step length
 R,S=64,4
 N = 2**S
-
+# f = 0.5
+model=[]
 def createFeatures( X ):
     # ln = X.shape[0]
     # return np.concatenate( (1-2*X,np.full((ln,1),1)),axis=1)
@@ -30,16 +31,111 @@ def mux_inputs(Z_train):
     p1,p2 = p1.astype(int), p2.astype(int)
     return p1,p2
 
-def prepare(p1,p2):
+def prepare(p1,p2,responses):
     ln = p1.shape[0]
-    err = []
     for i in range(ln):
         if p1[i]<p2[i]:
+            responses[i] = bool(1-responses[i])
             tmp = p1[i]
             p1[i] = p2[i]
             p2[i] = tmp
-            err.append(i)
-    return err
+
+def gog(ar,i,j,m):
+    l = [False for _ in range(0,i-j+1)]
+    l[0] = True
+    for k in range(1,i-j+1):
+        for jj in range(j,k+j):
+            if k==i-j and jj == j:
+                continue
+            if (ar[k+j][jj] == m) and l[jj-j]:
+                l[k] = True
+                break
+    return l[i-j]
+
+def get_unique(records_array):
+    idx_sort = np.argsort(records_array,axis=0)
+
+    # sorts records array so all unique elements are together
+    sorted_records_array = records_array[idx_sort]
+
+    # returns the unique values, the index of the first occurrence of a value, and the count for each element
+    vals, idx_start, count = np.unique(sorted_records_array, return_counts=True, return_index=True,axis=0)
+
+    # splits the indices into separate arrays
+    res = np.split(idx_sort, idx_start[1:])
+    return vals,res
+
+def find_path(ar,i,j):
+    if i-j < 2:
+        return -1
+    ans = 0
+    a = gog(ar,i,j,False)
+    b = gog(ar,i,j,True)
+    if a:
+        if b:
+            ans = 2
+        else:
+            ans = 0
+    else:
+        if b:
+            ans = 1
+        else:
+            ans = -1
+    return ans
+
+def add_predict(challenges,i,j,model):
+    l=[]
+    ln = challenges.shape[0]
+    arr = [[model[i][j].predict(challenges) if j<i else [0]*ln for j in range(N)] for i in range(N)]
+    arr = np.array(arr)
+    arr = arr.astype(bool)
+    for k in range(ln):
+        ar = arr[:,:,k]
+        # ar = np.squeeze(ar)
+        # print(ar.shape)
+        ans = find_path(ar,i,j)
+        # print(ans)
+        if ans > -1 and ans < 2:
+            l.append(bool(ans))
+        else:
+            l.append(ar[i][j])
+    return l
+
+def add_multi_predict(challenges,p1,p2,responses):
+    challenges = challenges.astype(str)
+    challenges = [''.join(cha) for cha in challenges]
+    challenges = np.array(challenges)
+    challenges_uq , idxs = get_unique(challenges)
+    print(challenges.shape,challenges_uq.shape)
+    nw_ch = []
+    nw_p1 = []
+    nw_p2 = []
+    nw_rs = []
+    ln = challenges_uq.shape[0]
+    for k in range(ln):
+        # print("go")
+        l = idxs[k]
+        ar = np.full((N,N),-1)
+        for i in l:
+            ar[p1[i]][p2[i]] = responses[i]
+        for i in range(N):
+            for j in range(i):
+                if ar[i][j] == -1:
+                    ans = find_path(ar,i,j)
+                    if ans==2:
+                        print(challenges_uq[k],i,j)
+                    if ans > -1 and ans < 2:
+                        ar[i][j] = bool(ans)
+                        nw_ch.append(idxs[k][0])
+                        nw_p1.append(i)
+                        nw_p2.append(j)
+                        nw_rs.append(bool(ans))
+                        # print("added",i,j)
+    return nw_ch,nw_p1,nw_p2,nw_rs
+
+
+
+
 
 ################################
 # Non Editable Region Starting #
@@ -60,11 +156,17 @@ def my_fit( Z_train ):
     challenges = createFeatures(Z_train[:,:R])
     responses = Z_train[:,-1]
     p1,p2 = mux_inputs(Z_train)
-    err = prepare(p1,p2)
-    responses[err] = 1 - responses[err]
+    prepare(p1,p2,responses)
+
+    # nw_ch,nw_p1,nw_p2,nw_rs = add_multi_predict(challenges,p1,p2,responses)
+
+    # challenges = np.append(challenges,challenges[nw_ch],axis=0)
+    # p1 = np.append(p1,nw_p1,axis=0)
+    # p2 = np.append(p2,nw_p2,axis=0)
+    # responses = np.append(responses,nw_rs,axis=0)
 
     ln = challenges.shape[0]
-    model = [[LogisticRegression() for j in range(i)]for i in range(N)]
+    model = [[LinearSVC() for j in range(i)]for i in range(N)]
     l =[[[]for j in range(i)] for i in range(N)]
     for i in range(ln):
         l[p1[i]][p2[i]].append(i)
@@ -90,17 +192,21 @@ def my_predict( X_tst, model ):
     X_tst = X_tst.astype(bool)
     challenges = createFeatures(X_tst[:,:R])
     p1,p2 = mux_inputs(X_tst)
-    l =[[[]for j in range(i)] for i in range(N)]
-    err = prepare(p1,p2)
+    l =[[[]for j in range(N)] for i in range(N)]
+    err = []
     for i in range(ln):
-        l[p1[i]][p2[i]].append(i)
+        if p1[i]-p2[i]<0:
+            err.append(i)
+        l[max(p1[i],p2[i])][min(p1[i],p2[i])].append(i)
     pred = np.empty(shape=(ln,))
     for i in range(N):
         for j in range(i):
             if l[i][j]:
                 ans = model[i][j].predict(challenges[l[i][j]])
+                # ans = add_predict(challenges[l[i][j]],i,j,model)
                 pred[l[i][j]] = ans
     pred[err] = 1- pred[err]
+    # print(pred)
     return pred
 
 if __name__ == "__main__":
